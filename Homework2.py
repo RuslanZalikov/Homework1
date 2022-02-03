5b7c99e70baa41a4f2fed7ade5d6faba18d3097c
ruslanzalikov <rus.zalikov@bk.ru>
2022-02-03 22:21:54 +0300

    Homeword 2

---
+++ Homework2.py
@@ -0,0 +1,8 @@
+
+def doesBrickFit(a, b, c, w, h):
+    data = [a, b, c]
+
+    data.pop(data.index(max(data)))
+    return min(data) <= min(w,h) and max(data) <= max(w,h)
+
+print(doesBrickFit(1, 2, 2, 1, 1))
